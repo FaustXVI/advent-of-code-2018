6ad99f0dfd88c329df4516a9f734814454e6e21d
FaustXVI <xavier.detant@gmail.com>
2018-12-02 23:43:56 +0100

    First puzzle done

--- day1/puzzle1/Calibration.hs
+++ day1/puzzle1/Calibration.hs
@@ -1,14 +1,16 @@
 module Calibration where
 
-curate :: String -> String
-curate s = map removePlus s
+curate :: String -> [String]
+curate s = words $ map removePlus s
     where removePlus :: Char -> Char
           removePlus '+' = ' '
           removePlus c = c
 
+sumNumbers :: [String] -> Int
+sumNumbers = foldl (+) 0 . map read
+
 calibrate :: (Monad m) => m String -> m Int
-calibrate input = fmap read curatedInput
-    where curatedInput = fmap curate input
+calibrate input = fmap (sumNumbers.curate) input 
 
 main :: IO ()
 main = fmap show (calibrate getContents) >>= putStrLn
--- day1/puzzle1/CalibrationSpecs.hs
+++ day1/puzzle1/CalibrationSpecs.hs
@@ -10,8 +10,12 @@ main = hspec $ do
     describe "Calibration" $ do
         it "should start at 0" $ do
             calibrate `withInput` "0" `shouldBe` 0
+            calibrate `withInput` "" `shouldBe` 0
         it "should add 1" $ do
             calibrate `withInput` "+1" `shouldBe` 1
         it "should remove 1" $ do
             calibrate `withInput` "-1" `shouldBe` (-1)
-
+        it "should add two numbers" $ do
+            calibrate `withInput` "1\n1" `shouldBe` 2
+        it "should add two negative numbers" $ do
+            calibrate `withInput` "-1\n-1" `shouldBe` (-2)
