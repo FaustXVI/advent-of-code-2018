31809416dab39aec7cac3ea139784c9c6b7610ef
FaustXVI <xavier.detant@gmail.com>
2018-12-02 23:36:36 +0100

    Parse one negative number inputs

--- day1/puzzle1/CalibrationSpecs.hs
+++ day1/puzzle1/CalibrationSpecs.hs
@@ -12,4 +12,6 @@ main = hspec $ do
             calibrate `withInput` "0" `shouldBe` 0
         it "should add 1" $ do
             calibrate `withInput` "+1" `shouldBe` 1
+        it "should remove 1" $ do
+            calibrate `withInput` "-1" `shouldBe` (-1)
 
