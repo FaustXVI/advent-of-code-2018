dfe81d71b742ede5abfe69965826d721eda7e9ab
FaustXVI <xavier.detant@gmail.com>
2018-12-03 18:40:19 +0100

    Add day2 first puzzle : checksum

--- day2/puzzle1/src/checksum.js
+++ day2/puzzle1/src/checksum.js
@@ -1,5 +1,26 @@
-function checksum(input){
-    return input;
+function letterCount(input) {
+    const result = {};
+    for (let i = 0; i < input.length; i++) {
+        result[input.charAt(i)] = (result[input.charAt(i)] || 0) + 1;
+    }
+    return result;
 }
 
-export {checksum};
+let containsALetterTimes = n => word => {
+    for (let letter in word) {
+        if (word[letter] === n) {
+            return true;
+        }
+    }
+    return false;
+};
+
+function checksum(input) {
+    const words = input.split("\n");
+    const letterCounts = words.map(letterCount);
+    const doubles = letterCounts.filter(containsALetterTimes(2)).length;
+    const triples = letterCounts.filter(containsALetterTimes(3)).length;
+    return doubles * triples;
+}
+
+export {checksum, letterCount, containsALetterTimes};
--- day2/puzzle1/src/checksum.spec.js
+++ day2/puzzle1/src/checksum.spec.js
@@ -1,9 +1,24 @@
 "use strict";
-import {checksum} from "./checksum"
+import {checksum, letterCount, containsALetterTimes} from "./checksum"
 import {expect} from "chai";
 
 describe("Checksum", () => {
-    xit("fails", () => {
+    it("should compute a full input", () => {
         expect(checksum("abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab\n")).to.equal(12);
+    });
+    it("should compute a simple input", () => {
+        expect(checksum("bababc\n")).to.equal(1);
+    });
+    describe("letter count", () => {
+        it("counts each letter", () => {
+            expect(letterCount("bababc")).to.deep.equal({"a": 2, "b": 3, "c": 1})
+        });
+    });
+    describe("predicate on letter duplication", () => {
+        it("find if one letter appears the right number of time", () => {
+            expect(containsALetterTimes(2)({"a": 2, "b": 3, "c": 1})).to.be.true;
+            expect(containsALetterTimes(3)({"a": 2, "b": 3, "c": 1})).to.be.true;
+            expect(containsALetterTimes(4)({"a": 2, "b": 3, "c": 1})).to.be.false;
+        });
     });
 });
